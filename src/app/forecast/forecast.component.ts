254843c5b91337b91165c28b2ccb3e6ea794630c
Josh Mobley <josh@joshmobley.net>
2018-04-18 23:37:50 -0500

    adding nighttime and morning statuses

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { ForecastService } from './forecast.service';
 
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 
 
 @Component({
@@ -22,6 +23,21 @@ export class ForecastComponent implements OnInit {
 
   getConditions(state,city) {
    return this.forecastService.conditions$(state,city);
+  }
+
+  getTime(timestamp) {
+    let date = timestamp.split(' ');
+    let hours = date[4].split(':')[0];
+    if (hours > 18) { return 'night'; }
+    if (hours > 16) { return 'evening'; }
+    if (hours > 10) { return 'day'; }
+    if (hours > 5) { return 'morning'; }
+    else { return 'night'; }
+  }
+
+  slugify(text) {
+    let str = text.replace(/\s/g, '');
+    return str.toLowerCase();
   }
 
   ngOnInit() {
